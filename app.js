54e315294ce290fa2ffecb1fbe7bd8f375a618ff
Menna Reda <menna.r712@gmail.com>
2025-02-25 10:37:22 +0200

    vercel deploy

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ connectDB();
 app.use(cookieParser());
 app.use(
     cors({
-      origin: ["https://keen-praline-992d7d.netlify.app", "http://localhost:5173"], 
+      origin: ["https://keen-praline-992d7d.netlify.app", "http://localhost:5173", "https://readify-kappa.vercel.app"], 
       credentials: true, 
     })
   );
